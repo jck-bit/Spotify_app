dd24d702b63a582d5aa31dc649300df0e27b2158
jck-bit <jckkinyanjui@gmail.com>
2022-02-06 01:36:20 +0300

    yooo

--- src/components/songs/Songs.jsx
+++ src/components/songs/Songs.jsx
@@ -11,17 +11,16 @@ import { MyList } from './Liked.styled';
 
 
 export default function Songs() {
-  return <div className='sing'>
+  return (
+   <>
       <div className="whole">
-        <div className="singTop">
            <div className="H1">
                <h1>
                    Good Evening
                </h1>
            </div>
-        </div>
-        <div className="singBottom">
-            <div className="List">
+       
+  
             <MyList>
                {Good.map((item, index) =>{
                return(
@@ -29,54 +28,9 @@ export default function Songs() {
                )
                })}
             </MyList>
-            
-                {/* <ul className='myList'>
-                    <li className="order">
-                        <div className="Icon">
-                          <img src="/assets/person/like.jpeg" alt="" className="iconImg" />
-                        </div>
-                    <span className='sidebarListItemText'>Liked songs</span>
-                    </li>
-                    <li className="order">
-                        <div className="Icon">
-                          <img src="/assets/person/nf.jpg" alt="" className="iconImg" />
-                        </div>
-                    <span className='sidebarListItemText'>NF Real Music</span>
-                    </li>
-                    <li className="order">
-                        <div className="Icon">
-                          <img src="/assets/person/selected.jpg" alt="" className="iconImg" />
-                        </div>
-                    <span className='sidebarListItemText'>Selected</span>
-                    </li>
-                    <li className="order">
-                        <div className="Icon">
-                          <img src="/assets/person/jb.jpg" alt="" className="iconImg" />
-                        </div>
-                    <span className='sidebarListItemText'>This is Justin Bieber</span>
-                    </li>
-                    <li className="order">
-                        <div className="Icon">
-                          <img src="/assets/person/2015.jpg" alt="" className="iconImg" />
-                        </div>
-                    <span className='sidebarListItemText'>2015 Playlist</span>
-                    </li>
-                    <li className="order">
-                        <div className="Icon">
-                          <img src="/assets/person/life_sucks.jpeg" alt="" className="iconImg" />
-                        </div>
-                    <span className='sidebarListItemText'>Life Sucks</span>
-                    </li>
-                    <li className="order">
-                        <div className="Icon">
-                          <img src="/assets/person/download.jpeg" alt="" className="iconImg" />
-                        </div>
-                    <span className='sidebarListItemText'>2010 Playlist</span>
-                    </li>
-                </ul> */}
-            </div>
-        </div>
-        </div>
+       
+
+       </div>
         <div className="anotherBottom">
          <div className="header">
                 <h1>Your Daily mixes</h1>
@@ -104,5 +58,6 @@ export default function Songs() {
                 )
             })}
        </Another>
-  </div>;
+  </>
+  );
 }
